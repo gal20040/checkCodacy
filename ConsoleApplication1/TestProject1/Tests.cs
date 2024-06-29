f62525d55455310563c7965023fba4ca41607a07
Artem Grodetskii <gal20040@gmail.com>
2024-06-29 17:50:38 +0200

    Initial commit.

---
+++ ConsoleApplication1/TestProject1/Tests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using static Calculator.Core.Calculator;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_int_and_return_correct_int()
+        {
+            const int expected = 3;
+            var actual = Add(1, 2);
+            
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void Minus_int_and_return_correct_int()
+        {
+            const int expected = -1;
+            var actual = Minus(1, 2);
+            
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
